8fa412a1cb0745608b225b9c8071ad5ea3b11e71
David Laganière <davidlag@gmail.com>
2020-06-24 22:11:52 -0400

    First working version!

--- app/app.py
+++ app/app.py
@@ -1,32 +1,76 @@
 import os
 from flask import Flask, abort, request, make_response, Response
+from flask import render_template, redirect, url_for, session
 from pprint import pprint
 
 app = Flask(__name__)
 
+app.secret_key = b'_8#y4L"F4Q5z\m\bgh]/'
+
 @app.route('/')
 def index():
-    resp = make_response("hello")
+    response = Response('', 401, {'WWW-Authenticate':'Basic realm="Login Required"'})
+    response.headers['X-Original-URI'] = request.headers.get('X-Original-URI')
 
-    print('Headers:', request.headers)
-    #abort(401)
-    #return 'Hello Flask from alpine-linux!'
-    #return Response(headers={'Authorized':'ApiKey Nk5XQWZISUI5eHhaR3FmdlJHa0E6UkszVnZYbGxUUjY3bEJyZmVkWGdXdw=='})
-    print('cookie:', request.cookies.get('kibana_auth'))
+    # As this is the entrypoint of web-auth, save the original URI to use
+    # it later to redirect the browser after the auth has passed. Only set
+    # it if empty to avoid overwriting it with subsequent calls to this function.
+    if session.get('logged_in'):
+        response = Response('', status=200, mimetype='application/json')
 
-    if request.cookies.get('kibana_auth') == None:
-        # Here we need to check the DB for a key and if not, request it from Elasticsearch.
-        # Then, we save the key in a cookie on the user's side so that it can be reused later.
+        # TODO: Use the username from a DB call to put in here instead of a static user.
+        response.headers['X-Jenkins-User'] = 'davidlag'
+        return response
 
-        # Set cookie
-        resp.set_cookie('kibana_auth', 'ZWxhc3RpYzpjaGFuZ2VtZQ==')
-        pprint(vars(resp))
-        return resp
     else:
-        print('found cookie:', request.cookies.get('kibana_auth'))
-        #return resp
-        return Response(headers={'Authorization': 'Basic ZWxhc3RpYzpjaGFuZ2VtZQ=='})
+        # As this is evaluated first, we need to return a 401 so that NGINX redirects
+        # to the login page.
+        return response
 
+    # If we ever get here, deny access.
+    return response
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Credentials are validated against the DB and if they are valid, the username
+    # only is saved. We would also validate credentials against LDAP or such another
+    # mean here as well.
+    if request.method == 'POST':
+        # TODO: Don't use static credentials but make an actual call to a DB.
+        if (request.form['username'] == 'blop') and (request.form['password'] == 'blop'):
+            response = make_response(redirect(request.args.get('url')))
+
+            session['username'] = request.form['username']
+            session['logged_in'] = True
+        
+            # Move forward to validate session and request Elasticsearch API key.
+            # TODO: Make the actual call to Elasticsearch API to get a new key.
+            session['kibana_auth'] = 'd3VHN2dYSUI5eHhaR3Fmdk1tVUs6b3hnVUJCTDFRMWVoaDhDNS1uTGlIQQ=='
+            response.set_cookie('kibana_auth', 'd3VHN2dYSUI5eHhaR3Fmdk1tVUs6b3hnVUJCTDFRMWVoaDhDNS1uTGlIQQ==')
+
+            return response
+
+        else:
+            # TODO: Better handling of this to give the user an error message.
+            return 'Wrong password!'
+    
+    # Login page is shown when the user does not POST any credentials.
+    return render_template('login.html', url=request.args.get('url'))
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session['logged_in'] = False
+    response = make_response(redirect(url_for('login', _external=True)))
+    response.set_cookie('kibana_auth', expires=0)
+    return response
+
+# TODO: Build this function to do the actual DB call.
+def authenticate(username, password):
+    '''
+    Authenticate the user against the local DB using the provided
+    credentials.
+    '''
+    return 'Credentials:' + username + ':' + password
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 6000.
