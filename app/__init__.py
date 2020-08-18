641654d7d321e8e6aa28f8092ee2485d0161c545
David Laganière <davidlag@gmail.com>
2020-08-18 10:35:58 -0400

    Added option for various configs we could use

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,20 @@ def create_app(config_filename=None):
     app.permanent_session_lifetime = timedelta(minutes=SESSION_EXPIRATION)
     register_blueprints(app)
 
+    # TODO: Investigate further how to use this.
+    # Dynamically load config based on the testing argument or FLASK_ENV environment variable
+    """
+    flask_env = os.getenv("FLASK_ENV", None)
+    if testing:
+        app.config.from_object(config.TestingConfig)
+    elif flask_env == "development":
+        app.config.from_object(config.ProductionConfig)
+    elif flask_env == "testing":
+        app.config.from_object(config.TestingConfig)
+    else:
+        app.config.from_object(config.ProductionConfig)
+    """
+
     # Bind to PORT if defined, otherwise default to 6000.
     #port = int(os.environ.get('PORT', 6000))
     #app.run(host='0.0.0.0', port=port, debug=True)
