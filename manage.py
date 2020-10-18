a73052dba3771be3a821a24f1b34079b42ae5167
David Laganière <davidlag@gmail.com>
2020-10-18 05:24:29 -0400

    Patch to cover a very old bug related to code coverage

--- manage.py
+++ manage.py
@@ -8,6 +8,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webtools.settings')
     try:
+        # pylint: disable=import-outside-toplevel
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
@@ -15,7 +16,26 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # Code coverage is handled here because of a bug with Django and nose that
+    # hasn't been fixed after years.
+    is_testing = 'test' in sys.argv[1]
+
+    if is_testing:
+        # pylint: disable=import-outside-toplevel
+        from coverage import Coverage
+        cov = Coverage()
+        cov.erase()
+        cov.start()
+
     execute_from_command_line(sys.argv)
+
+    if is_testing:
+        cov.stop()
+        cov.save()
+        cov.report()
+        cov.html_report(directory='htmlcov')
+        cov.xml_report()
 
 
 if __name__ == '__main__':
